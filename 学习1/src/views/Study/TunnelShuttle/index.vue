c361a7d023d9d28bc7a7874d8947e3b0c8785b75
付祖敏 <1326580471@qq.com>
2025-08-18 16:12:30 +0800

    增加示例

--- 学习1/src/views/Study/TunnelShuttle/index.vue
+++ 学习1/src/views/Study/TunnelShuttle/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>1</div>
+  <div class="box"></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { TunnelShuttleClass } from "./utils";
+
+const TunnelShuttleClassRef = ref<TunnelShuttleClass>();
+
+onMounted(() => {
+  setTimeout(() => {
+    const boxDom = document.querySelector(".box")! as HTMLElement;
+    TunnelShuttleClassRef.value = new TunnelShuttleClass(boxDom);
+  }, 1000);
+});
+
+onBeforeUnmount(() => {
+  // console.log("销毁组件");
+  TunnelShuttleClassRef.value.destroy();
+});
+</script>
 
 <style lang="scss" scoped></style>
